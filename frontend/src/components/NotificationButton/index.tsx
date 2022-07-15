8017f65e19d739e08fbb2ee4279bffaa24de83a3
Lucas Previato <lucaspreviato@live.com>
2022-07-14 23:52:21 -0300

    Layout button

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+
+import icon from '../../assets/img/notification-icon.svg'
+
+export function NotificationButton() {
+
+
+  return (
+    <div className="h-11 w-11 bg-[rgba(255,107,114, 0.1)] border-2 rounded-xl border-solid border-[#ff6b72] flex items-center justify-center cursor-pointer">
+      <img className="w-5" src={icon} alt=" fjfjf" />
+    </div>
+  )
+}
