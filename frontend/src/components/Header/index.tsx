9b61e8a52b2686a3377414f74a19c5ed3f4e94ac
Lucas Previato <lucaspreviato@live.com>
2022-07-15 17:48:19 -0300

    Start header component development

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,15 @@
+
+
+export function Header(){
+
+
+    return(
+        <header>
+            <div>
+                <img src="logo.svg" alt="" />
+                <h1>DSMeta</h1>
+                <p>Desenvolvido por <a href="#">@LucasPreviato</a></p>
+            </div>
+        </header>
+    )
+}
